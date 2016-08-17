33422ff8644bf5b39eff7a54b3e92f229ee85593
alingse <alingse@foxmail.com>
2016-08-17 14:52:58 +0800

    add:添加站点的一些配置const

---
+++ sites/utils.py
@@ -0,0 +1,26 @@
+
+
+class config(object):
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError, "Can't reset the const{0}".format(name)
+        self.__dict__[name] = value
+
+    def __getattr__(self, name):
+        if name in self.__dict__:
+            return self.__dict__[name]
+        return None
+
+
+class _site(config):
+    def __init__(self):
+        super(_site,self).__init__()
+        #self.__setattr__('func',self.func)
+
+_tails = ['.js','.css','.jpg','.jpeg','.png','.gif','.svg','.pdf','.icon','.mp3']
+_tails_set = set(_tails+map(str.upper,_tails))
+        
+if __name__ == '__main__':
+    pass
+    #print(invalid_tails)
