630d2252287037115fab29c8e414a53e76246fdc
alingse <alingse@foxmail.com>
2016-08-22 14:26:40 +0800

    update:修改runutil增加线程工作单元

---
+++ runutil.py
@@ -0,0 +1,60 @@
+#coding=utf-8
+#author@aingse
+#2016.08.21
+
+from threading import Thread
+from Queue import Empty,Queue
+from time import sleep
+from operator import truth
+
+#从一端取再放入另一端
+#或者是注册函数
+class runUnit(Thread):
+
+    def __init__(self,execute,diff = 0.1,**kwargs):
+        super(runUnit,self).__init__()
+        self.execute = execute
+        self.diff = diff
+        self.kwargs = kwargs
+        self.exit = False
+        self.exited = False
+        
+
+    def shutdown(self):
+        self.exit = True
+
+    def run(self):
+        while not self.exit:
+            self.execute(**self.kwargs)
+            sleep(self.diff)
+        #exit
+        self.exited = True
+
+
+def exit_all(runlist):
+    for unit in runlist:
+        unit.shutdown()
+
+
+def all_exited(runlist):
+    runs = filter(truth,[u.exited for u in runlist])
+    if runs == []:
+        return True
+    return len(runs)
+
+
+if __name__ == '__main__':
+    def execute(a=4,b=2):
+        print(a,b)
+
+    runlist = []
+    for i in range(3):
+        u = runUnit(execute,a=3,b=7)
+        u.start()
+        runlist.append(u)
+    sleep(5)
+    exit_all(runlist)
+    while all_exited(runlist) != True:
+        sleep(0.1)
+
+
